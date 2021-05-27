45e76da6d73793558f09f075b30ad14847260e5e
Kamontat Chantrachirathumrong <developer@kamontat.net>
2021-05-27 16:10:58 +0700

    Update start.sh

--- ftgenerator/start.sh
+++ ftgenerator/start.sh
@@ -1,1 +1,261 @@
-echo "test"
+#!/usr/bin/env bash
+# shellcheck disable=SC1000
+
+# generate by 2.3.2
+# link (https://github.com/Template-generator/script-genrating/tree/2.3.2)
+
+set -e #ERROR    - Exit whole scripts if single non-zero command return
+# set -x #DEBUG    - Display commands and their arguments as they are executed.
+# set -v #VERBOSE  - Display shell input lines as they are read.
+# set -n #EVALUATE - Check syntax of the script but don't execute.
+
+##################################################################################
+## 1.   (optional) run setup.sh for setup environment and install dependencies  ##
+## 2.1. (optional) fetch ftgenerator <version> from github private repository   ##
+## 2.2. (optional) install ftgenerator scripts and data to correctly location   ##
+## 3.   (optional) fetch latest freqtrade code from Github                      ##
+## 4.   (optional) start ftgenerator script                                     ##
+##################################################################################
+
+# @helper
+throw() {
+  printf '%s\n' "$1" >&2 && exit "$2"
+  return 0
+}
+
+# @helper
+throw_if_empty() {
+  local text="$1"
+  test -z "$text" && throw "$2" "$3"
+  return 0
+}
+
+# @option
+require_argument() {
+  throw_if_empty "$LONG_OPTVAL" "'$LONG_OPTARG' require argument" 9
+}
+
+# @option
+no_argument() {
+  [[ -n $LONG_OPTVAL ]] && ! [[ $LONG_OPTVAL =~ "-" ]] && throw "$LONG_OPTARG don't have argument" 9
+  OPTIND=$((OPTIND - 1))
+}
+
+# @syscall
+set_key_value_long_option() {
+  if [[ $OPTARG =~ "=" ]]; then
+    LONG_OPTVAL="${OPTARG#*=}"
+    LONG_OPTARG="${OPTARG%%=$LONG_OPTVAL}"
+  else
+    LONG_OPTARG="$OPTARG"
+    LONG_OPTVAL="$1"
+    OPTIND=$((OPTIND + 1))
+  fi
+}
+
+banner() {
+  echo "----------------------------------------"
+  echo "$@"
+  echo "----------------------------------------"
+}
+
+APP_NAME="ftgenerator"
+FREQTRADE_DIRECTORY="/etc/freqtrade"
+FT_GENERATOR_DIRECTORY="/etc/$APP_NAME"
+DOCKER_DATA_DIRECTORY="/etc/ftdata"
+
+SETUP_MODE=false     # setup environment
+FTG_FETCH_MODE=false # download ftgenerator
+FT_FETCH_MODE=false  # fetch latest version of freqtrade
+FTG_START_MODE=false # start ftgenerator
+
+__setup_msg="[-U|--setup]"
+__fetch_ftg_msg="[-G|--fetch-ftg] [(-t|--token) <token>] [(-v|--version) <v0.0.0>]"
+__fetch_ft_msg="[-F|--fetch-ft]"
+__start_ftg_msg="[-S|--start-ftg]"
+
+__help_msg="start.sh $__setup_msg $__fetch_ft_msg $__fetch_ftg_msg $__start_ftg_msg"
+
+load_option() {
+  while getopts 't:v:UGFS-:' flag; do
+    case "${flag}" in
+    U) SETUP_MODE=true ;;
+    G) FTG_FETCH_MODE=true ;;
+    F) FT_FETCH_MODE=true ;;
+    S) FTG_START_MODE=true ;;
+    t) TOKEN="$OPTARG" ;;
+    v) APP_VERSION="$OPTARG" ;;
+    -)
+      export LONG_OPTARG
+      export LONG_OPTVAL
+      NEXT="${!OPTIND}"
+      set_key_value_long_option "$NEXT"
+      case "${OPTARG}" in
+      setup)
+        no_argument
+        SETUP_MODE=true
+        ;;
+      fetch-ftg)
+        no_argument
+        FTG_FETCH_MODE=true
+        ;;
+      start-ftg)
+        no_argument
+        FTG_START_MODE=true
+        ;;
+      fetch-ft)
+        no_argument
+        FT_FETCH_MODE=true
+        ;;
+      token)
+        require_argument
+        TOKEN="$LONG_OPTVAL"
+        ;;
+      version)
+        require_argument
+        APP_VERSION="$LONG_OPTVAL"
+        ;;
+      *)
+        # because optspec is assigned by 'getopts' command
+        # shellcheck disable=SC2154
+        if [ "$OPTERR" == 1 ] && [ "${optspec:0:1}" != ":" ]; then
+          echo "Unexpected option '$LONG_OPTARG', $__help_msg" >&2
+          exit 9
+        fi
+        ;;
+      esac
+      ;;
+    ?)
+      echo "Unexpected option, $__help_msg" >&2
+      exit 10
+      ;;
+    *)
+      echo "Unexpected option $flag, $__help_msg" >&2
+      exit 10
+      ;;
+    esac
+  done
+}
+
+load_option "$@"
+OS="darwin"
+if [[ $(uname -s) != "Darwin" ]]; then
+  OS="linux"
+fi
+
+if $SETUP_MODE && [[ "$OS" == "linux" ]]; then
+  banner "Start setup mode"
+
+  __reset_root_mode=false
+  echo "Do you want to reset 'root' password [Y|n]: "
+  read -r ans
+  if [[ "$ans" == "Y" ]] || [[ "$ans" == "y" ]]; then
+    __reset_root_mode=true
+  fi
+
+  __installation_list=(
+    "python3-pip"
+    "python3-pandas"
+    "git"
+    "vim"
+    "apt-transport-https"
+    "curl"
+    "ca-certificates"
+    "gnupg"
+  )
+
+  banner "Update and install new dependencies"
+  sudo apt update
+  sudo apt upgrade
+  sudo apt install -y "${__installation_list[@]}"
+
+  banner "Install docker"
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+  echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+  sudo apt update
+  sudo apt install -y "docker-ce" "docker-ce-cli" "containerd.io"
+
+  banner "Install docker-compose"
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+
+  if $__reset_root_mode; then
+    banner "Reset 'root' password"
+    sudo passwd root
+  fi
+
+  unset __installation_list __reset_root_mode
+fi
+
+if $FT_FETCH_MODE; then
+  if command -v "git" >/dev/null; then
+    git clone 'https://github.com/freqtrade/freqtrade.git' "$FREQTRADE_DIRECTORY"
+  fi
+fi
+
+if $FTG_FETCH_MODE || $FTG_START_MODE; then
+  banner "Start ftgenerator fetch=$FTG_FETCH_MODE and start=$FTG_START_MODE"
+
+  if test -z "$APP_VERSION"; then
+    echo "needs ftgenerator version, --help for more information"
+    exit 1
+  fi
+
+  __github_api="https://api.github.com/graphql"
+  __github_owner="kamontat"
+  __github_repo="$APP_NAME"
+  __output_ftgenerator="$FT_GENERATOR_DIRECTORY/$APP_VERSION/"
+
+  banner "Create ftgenerator on $__output_ftgenerator"
+
+  if $FTG_FETCH_MODE; then
+    if ! test -d "$__output_ftgenerator"; then
+      if test -z "$TOKEN"; then
+        echo "needs github token, --help for more information"
+        exit 1
+      fi
+
+      __repo_tar="$__github_repo-$OS-${APP_VERSION#*v}.tar.gz" # remove v prefix in version
+
+      __gh_get_asset() {
+        query="{\"query\": \"{repository(owner: \\\"$__github_owner\\\", name: \\\"$__github_repo\\\"){release(tagName: \\\"$APP_VERSION\\\") {releaseAssets(name: \\\"$__repo_tar\\\", first: 1) {edges{node{url}}}}}}\"}"
+
+        curl -sL -X POST \
+          --header "Authorization: token $TOKEN" --header "Accept: application/vnd.github.v3.raw" \
+          -d "$query" "$__github_api"
+      }
+
+      __curl() {
+        curl --output "$__repo_tar" -L "$@"
+      }
+
+      asset_json="$(__gh_get_asset)"
+      echo "github response: $asset_json"
+
+      trim_asset_json="${asset_json%\"*}"
+      asset_link="${trim_asset_json##*\"}"
+      echo "asset url: $asset_link"
+
+      __curl "$asset_link"
+      echo "download to $__repo_tar"
+
+      mkdir -p "$__output_ftgenerator" # create directory
+      tar -xzvf "$__repo_tar" -C "$__output_ftgenerator"
+      rm -r "$__repo_tar"
+
+      unset asset_json trim_asset_json asset_link
+      unset __repo_tar
+    else
+      echo "ftgenerator version $APP_VERSION is already exist"
+    fi
+  fi
+
+  cd "$__output_ftgenerator" || exit 1
+
+  if $FTG_START_MODE; then
+    ./ftgenerator --version
+    ./ftgenerator --level 3 --pwd "$FREQTRADE_DIRECTORY" --user-data "$FREQTRADE_DIRECTORY/user_data" --docker "$DOCKER_DATA_DIRECTORY"
+  fi
+
+  unset __github_api __github_owner __github_repo __output_ftgenerator
+fi
